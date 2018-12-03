155f61199cf3decf4e1b7bf1861b36ea9e2402cf
simoncropp <simon.cropp@gmail.com>
2018-12-03 13:43:50 +1100

    .

--- src/RepoSync/Program.cs
+++ src/RepoSync/Program.cs
@@ -15,24 +15,24 @@ class Program
         sync.AddSourceItem(TreeEntryTargetType.Blob, "src/RepoSync/Source/ISSUE_TEMPLATE/bug_report.md", ".github/ISSUE_TEMPLATE/bug_report.md");
         sync.AddSourceItem(TreeEntryTargetType.Blob, "src/RepoSync/Source/ISSUE_TEMPLATE/feature_request.md", ".github/ISSUE_TEMPLATE/feature_request.md");
         sync.AddSourceItem(TreeEntryTargetType.Blob, "src/RepoSync/Source/pull_request_template.md", ".github/pull_request_template.md");
-        sync.AddTarget("NServiceBusExtensions", "NServiceBus.Wire", "master");
-        sync.AddTarget("NServiceBusExtensions", "NServiceBus.ZeroFormatter", "master");
-        sync.AddTarget("NServiceBusExtensions", "NServiceBus.Utf8Json", "master");
         sync.AddTarget("NServiceBusExtensions", "NServiceBus.Attachments", "master");
-        sync.AddTarget("NServiceBusExtensions", "NServiceBus.MessagePack", "master");
-        sync.AddTarget("NServiceBusExtensions", "NServiceBus.Hyperion", "master");
+        sync.AddTarget("NServiceBusExtensions", "NServiceBus.AuditFilter", "master");
         sync.AddTarget("NServiceBusExtensions", "NServiceBus.Bond", "master");
+        sync.AddTarget("NServiceBusExtensions", "NServiceBus.MessagePack", "master");
+        sync.AddTarget("NServiceBusExtensions", "NServiceBus.HandlerOrdering", "master");
+        sync.AddTarget("NServiceBusExtensions", "NServiceBus.Hyperion", "master");
         sync.AddTarget("NServiceBusExtensions", "Newtonsoft.Json.Encryption", "master");
         sync.AddTarget("NServiceBusExtensions", "NServiceBus.Jil", "master");
-        sync.AddTarget("NServiceBusExtensions", "NServiceBus.MsgPack", "master");
-        sync.AddTarget("NServiceBusExtensions", "NServiceBus.AuditFilter", "master");
-        sync.AddTarget("NServiceBusExtensions", "NServiceBus.HandlerOrdering", "master");
         sync.AddTarget("NServiceBusExtensions", "NServiceBus.MicrosoftLogging", "master");
-        sync.AddTarget("NServiceBusExtensions", "NServiceBus.Serilog", "master");
+        sync.AddTarget("NServiceBusExtensions", "NServiceBus.MsgPack", "master");
         sync.AddTarget("NServiceBusExtensions", "NServiceBus.Native", "master");
         sync.AddTarget("NServiceBusExtensions", "NServiceBus.ProtoBufGoogle", "master");
         sync.AddTarget("NServiceBusExtensions", "NServiceBus.ProtoBufNet", "master");
         sync.AddTarget("NServiceBusExtensions", "NServiceBus.Validation", "master");
+        sync.AddTarget("NServiceBusExtensions", "NServiceBus.Serilog", "master");
+        sync.AddTarget("NServiceBusExtensions", "NServiceBus.Utf8Json", "master");
+        sync.AddTarget("NServiceBusExtensions", "NServiceBus.Wire", "master");
+        sync.AddTarget("NServiceBusExtensions", "NServiceBus.ZeroFormatter", "master");
         return sync.Sync();
     }
 }
